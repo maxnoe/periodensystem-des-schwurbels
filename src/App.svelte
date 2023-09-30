dae95b0ed268be756f480be1c46e008bff6d9f8b
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-09-30 14:46:43 +0200

    Start playing around

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+import PeriodicSystem from "./PeriodicSystem.svelte"
+</script>
+
+<main>
+    <h1>Das Periodensytem des Schwurbels</h1>
+    <div class="wrapper">
+        <PeriodicSystem />
+    </div>
+</main>
+
+<style>
+    .wrapper {
+        display: block;
+    }
+</style>
---
+++ src/ElementTile.svelte
@@ -0,0 +1,22 @@
+<script>
+export let element = "";
+export let color = "";
+</script>
+
+<main style="--color: {color}">
+    <span id="element">{ element }</span>
+</main>
+
+<style>
+    #element {
+        font-size: 2rem;
+    }
+
+    main {
+        display: inline-block;
+        width: 4rem;
+        height: 4rem;
+        background-color: var(--color);
+        margin: 5pt;
+    }
+</style>
