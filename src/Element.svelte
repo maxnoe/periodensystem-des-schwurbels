0446cd89840e23bd6d3710a7688efa43936d6a97
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-11-06 17:34:05 +0100

    More schwurbel

--- src/Element.svelte
+++ src/Element.svelte
@@ -1,15 +1,20 @@
 <script>
 import data from "./schwurbel.json";
+import ElementTile from "./ElementTile.svelte"
+import { elements } from "./PeriodicTable.json"
 
 const params = new URLSearchParams(window.location.search);
 const element = params.get("element");
 const items = data[element];
+const element_data = elements.find((e) => e.symbol == element);
 
 </script>
 
 <main>
     <h1><a href="/">Das Periodensytem des Schwurbels</a></h1>
-    <h2>{ element }</h2>
+    <h2>
+        <ElementTile element={element_data} items={items}  />
+    </h2>
 
     {#if items === undefined}
     <p>Noch kein Schwurbel eingetragen oder bei Minkorrekt besprochen.</p>
@@ -21,12 +26,11 @@ const items = data[element];
         <p>{item.abstract}</p>
         <ul>
             <li>
-                <a href="{item.url}" target="_blank">Schwurbel-Website</a>
+                Schwurbel-Website: <a href="{item.url}" target="_blank">{item.url}</a>
             </li>
             <li>
-                <a href="{item.minkorrekt_url}">
-                    Besprochen in Methodisch Inkorrekt Folge {item.schwurbel_der_woche}
-                </a>
+                Besprochen in Methodisch Inkorrekt Folge:
+                <a href="{item.minkorrekt_url}" target="_blank">{item.schwurbel_der_woche} </a>
             </li>
         </ul>
     </div>
@@ -37,5 +41,11 @@ const items = data[element];
 <style>
 .card {
     text-align: left;
+}
+
+h2 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 </style>
